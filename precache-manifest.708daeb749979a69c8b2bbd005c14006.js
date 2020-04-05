6f0426964dbb243aac296636e2d9187f490f8e18
seonjuleee <nlsj0820@naver.com>
2020-04-05 21:27:25 +0900

    Updates

---
+++ precache-manifest.708daeb749979a69c8b2bbd005c14006.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47873ab0f1252aec83aa4aad47379416",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "6d73f081a197b8f8ce90",
+    "url": "/movie_app_2020/static/css/main.71abf510.chunk.css"
+  },
+  {
+    "revision": "d81438a4d598e83349fe",
+    "url": "/movie_app_2020/static/js/2.8d5eb2b8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.8d5eb2b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d73f081a197b8f8ce90",
+    "url": "/movie_app_2020/static/js/main.ee36df85.chunk.js"
+  },
+  {
+    "revision": "d5ba99470fbd5d5276a4",
+    "url": "/movie_app_2020/static/js/runtime-main.6a93e83b.js"
+  }
+]);
